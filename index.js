8a5509bfd5f28577012c3dc4d8e03af598d804c1
charlotte karimi <[charlotte.karimi@student.moringaschool.com]>
2022-12-16 16:49:00 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  const lowerCasedString = word.toLowerCase()
+  const splitString = lowerCasedString.split("")
+  const splitReverseString = splitString.reverse()
+  const joinString = splitReverseString.join("");
+  if(lowerCasedString !== joinString)
+  {
+    return false
+  }else
+    return true
 }
 
 /* 
   Add your pseudocode here
+  write string input
+  convert to lowercase
+  reverse string
+  compare strings
+  return 'true'if string is equal
+  return 'false'if string isn't equal
 */
 
 /*
   Add written explanation of your solution here
+  I turned the string to lowercase
+  i split the string into arrays
+  i reversed the split string
+  i rejoined the reversed string and compared it to the original string
+
 */
 
 // You can run `node index.js` to view these console logs
